0fbae510a6d4fca91f54e5a306b30e9354b508bd
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-04-20 13:17:43 +0100

    New post page added

---
+++ src/components/Newpost.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="new-post">
+    <h2>New Post</h2>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/styles/vars';
+
+  .new-post {
+    margin-top: 6em;
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import VueRouter from 'vue-router';
 import Cardlist from './components/Cardlist.vue'
 import Card from './components/Card.vue'
 import User from './components/User.vue'
+import Newpost from './components/Newpost.vue'
 
 const router = new VueRouter({
   mode: 'history',
@@ -9,7 +10,8 @@ const router = new VueRouter({
     { path: '/', component: Cardlist},
     { path: '/cards', component: Cardlist },
     { path: '/card/:id', component: Card},
-    { path: '/user', component: User}
+    { path: '/user', component: User},
+    { path: '/newpost', component: Newpost}
   ],
 });
 
